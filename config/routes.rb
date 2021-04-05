f6a1f0231ee2b91a4f784fe51ace65e42cbc9582
user <aax.chiri@gmail.com>
2021-04-05 21:15:26 +0900

    eventモデルを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  resources :events
   root 'welcome#index'
   get '/auth/:provider/callback' => 'sessions#create'
   delete 'logout' => 'sessions#destroy'
