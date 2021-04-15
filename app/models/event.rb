f2f1cca869ed02e5cae9cd899a5c8cad484d4f1f
user <aax.chiri@gmail.com>
2021-04-15 10:24:06 +0900

    画像のファイルサイズと幅に関するバリデーションを追加

--- app/models/event.rb
+++ app/models/event.rb
@@ -9,8 +9,11 @@ class Event < ApplicationRecord
 
   validates :name, length: { maximum: 50 }, presence: true
   validates :place, length: { maximum: 100 }, presence: true
-  validates :image, content_type: %i[png jpg jpeg]
-  validates :content, length: {maximum: 2000 }, presence: true
+  validates :image,
+            content_type: %i[png jpg jpeg],
+            size: { less_than_or_equal_to: 10.megabytes },
+            dimension: { width: { max: 2000 }, height: { max: 2000 } }
+  validates :content, length: { maximum: 2000 }, presence: true
   validates :start_at, presence: true
   validates :end_at, presence: true
   validate :start_at_should_be_before_end_at
